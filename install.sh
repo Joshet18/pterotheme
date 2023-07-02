3e9f0ff100315ab5f4ffd05029ad8ab6853059a9
Joshet18 <74809921+Joshet18@users.noreply.github.com>
2023-07-02 00:03:57 -0500

    Code

--- install.sh
+++ install.sh
@@ -47,7 +47,7 @@ installThemeQuestion(){
 }
 
 repair(){
-    bash <(curl https://raw.githubusercontent.com/Angelillo15/MinecraftPurpleTheme/main/repair.sh)
+    bash <(curl https://raw.githubusercontent.com/Joshet18/pterotheme/main/repair.sh)
 }
 
 restoreBackUp(){
