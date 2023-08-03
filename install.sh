8a7805d46177de718ded7de6b19787c59c4e011f
Joshet18 <74809921+Joshet18@users.noreply.github.com>
2023-08-02 22:46:44 -0500

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,11 +1,40 @@
 #!/bin/bash
 
+green="\e[0;32m\033[1m"
+white='\033[0;37m'
+black='\033[0;30m'
+clear='\033[0m'
+red="\e[0;31m\033[1m"
+purple="\e[0;35m\033[1m"
+magenta='\033[0;35m'
+cyan='\033[0;36m'
+gray="\e[0;37m\033[1m"
+blue="\e[0;34m\033[1m"
+yellow="\e[0;33m\033[1m"
+
 if (( $EUID != 0 )); then
     echo "Please run as root"
     exit
 fi
 
 clear
+
+installPanel(){
+    apt -y install software-properties-common curl apt-transport-https ca-certificates gnupg
+    LC_ALL=C.UTF-8 add-apt-repository -y ppa:ondrej/php
+    curl -fsSL https://packages.redis.io/gpg | sudo gpg --dearmor -o /usr/share/keyrings/redis-archive-keyring.gpg
+    echo "deb [signed-by=/usr/share/keyrings/redis-archive-keyring.gpg] https://packages.redis.io/deb $(lsb_release -cs) main" | sudo tee /etc/apt/sources.list.d/redis.list
+    curl -sS https://downloads.mariadb.com/MariaDB/mariadb_repo_setup | sudo bash
+    apt update
+    apt-add-repository universe
+    apt -y install php8.1 php8.1-{common,cli,gd,mysql,mbstring,bcmath,xml,fpm,curl,zip} mariadb-server nginx tar unzip git redis-server
+    curl -sS https://getcomposer.org/installer | sudo php -- --install-dir=/usr/local/bin --filename=composer
+    mkdir -p /var/www/pterodactyl
+    cd /var/www/pterodactyl
+    curl -Lo panel.tar.gz https://github.com/pterodactyl/panel/releases/latest/download/panel.tar.gz
+    tar -xzvf panel.tar.gz
+    chmod -R 755 storage/* bootstrap/cache/
+}
 
 installTheme(){
     cd /var/www/
@@ -47,6 +76,17 @@ installTheme(){
 
 installThemeQuestion(){
     while true; do
+        read -p "Are you sure that you want to install the panel [y/n]? " yn
+        case $yn in
+            [Yy]* ) installPanel; break;;
+            [Nn]* ) exit;;
+            * ) echo "Please answer yes or no.";;
+        esac
+    done
+}
+
+installPanelQuestion(){
+    while true; do
         read -p "Are you sure that you want to install the theme [y/n]? " yn
         case $yn in
             [Yy]* ) installTheme; break;;
@@ -72,25 +112,30 @@ restoreBackUp(){
 }
 echo "Pterodactyl Theme Installer"
 echo ""
-echo "[1] Install theme"
-echo "[2] Restore backup"
-echo "[3] Repair panel (use if you have an error in the theme installation)"
+echo "[1] Install Panel"
+echo "[2] Install theme"
+echo "[3] Restore backup"
+echo "[4] Repair panel (use if you have an error in the theme installation)"
 echo "[4] Exit"
 
 read -p "Please enter a number: " choice
 if [ $choice == "1" ]
     then
-    installThemeQuestion
+    installPanelQuestion
 fi
 if [ $choice == "2" ]
     then
-    restoreBackUp
+    installThemeQuestion
 fi
 if [ $choice == "3" ]
     then
-    repair
+    restoreBackUp
 fi
 if [ $choice == "4" ]
+    then
+    repair
+fi
+if [ $choice == "5" ]
     then
     exit
 fi
