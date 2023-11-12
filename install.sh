69a5d8636b7f70426e59602b477f0102dd8d09c6
Joshet18 <74809921+Joshet18@users.noreply.github.com>
2023-11-11 22:31:21 -0500

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,16 +1,34 @@
 #!/bin/bash
 
+orange="\033[0;33m"
+green="\e[0;32m\033[1m"
+gray='\033[0;37m'
+black='\033[0;30m'
+clear='\033[0m'
+lightred="\033[1;31m"
+red="\033[0;31m"
+lightpurple="\033[1;35m"
+purple="\033[0;35m"
+cyan="\033[0;36m"
+lightcyan="\033[1;36m"
+white="\e[0;37m\033[1m"
+blued="\033[1;34m"
+blue="\e[0;34m\033[1m"
+yellow="\e[0;33m\033[1m"
+
 if (( $EUID != 0 )); then
-    echo "Please run as root"
+    echo -e "${red}Please run as root"
     exit
 fi
 
 clear
 
-installTheme(){
+
+
+Theme(){
+    echo -e "${green}Instalando Tema..."
     cd /var/www/
     tar -cvf Backup.tar.gz pterodactyl
-    echo "Installing theme..."
     cd /var/www/pterodactyl
     rm -r pterotheme
     git clone https://github.com/Joshet18/pterotheme.git
@@ -41,19 +59,43 @@ installTheme(){
     cd /var/www/pterodactyl
     yarn build:production
     sudo php artisan optimize:clear
-
-
 }
 
-installThemeQuestion(){
+ThemeQuestion(){
     while true; do
-        read -p "Are you sure that you want to install the theme [y/n]? " yn
+        echo -ne "${cyan}Estas seguro de instalar el tema"; read -p " [y/n]? " yn
         case $yn in
-            [Yy]* ) installTheme; break;;
+            [Yy]* ) Theme; break;;
             [Nn]* ) exit;;
-            * ) echo "Please answer yes or no.";;
+            * ) echo -e "${red}Elije entre yes o no.";;
         esac
     done
+}
+
+PterodactylDependencies(){
+    apt -y install software-properties-common curl apt-transport-https ca-certificates gnupg
+    LC_ALL=C.UTF-8 add-apt-repository -y ppa:ondrej/php
+    curl -fsSL https://packages.redis.io/gpg | sudo gpg --dearmor -o /usr/share/keyrings/redis-archive-keyring.gpg
+    echo "deb [signed-by=/usr/share/keyrings/redis-archive-keyring.gpg] https://packages.redis.io/deb $(lsb_release -cs) main" | sudo tee /etc/apt/sources.list.d/redis.list
+    curl -sS https://downloads.mariadb.com/MariaDB/mariadb_repo_setup | sudo bash
+    apt update
+    apt -y install php8.1 php8.1-{common,cli,gd,mysql,mbstring,bcmath,xml,fpm,curl,zip} mariadb-server nginx tar unzip git redis-server
+    curl -sS https://getcomposer.org/installer | sudo php -- --install-dir=/usr/local/bin --filename=composer
+
+    mkdir -p /var/www/pterodactyl
+    cd /var/www/pterodactyl
+    curl -Lo panel.tar.gz https://github.com/pterodactyl/panel/releases/latest/download/panel.tar.gz
+    tar -xzvf panel.tar.gz
+    chmod -R 755 storage/* bootstrap/cache/
+}
+
+WingsDependencies(){
+    curl -sSL https://get.docker.com/ | CHANNEL=stable bash
+    sudo systemctl enable --now docker
+
+    sudo mkdir -p /etc/pterodactyl
+    curl -L -o /usr/local/bin/wings "https://github.com/pterodactyl/wings/releases/latest/download/wings_linux_$([[ "$(uname -m)" == "x86_64" ]] && echo "amd64" || echo "arm64")"
+    sudo chmod u+x /usr/local/bin/wings
 }
 
 repair(){
@@ -61,7 +103,7 @@ repair(){
 }
 
 restoreBackUp(){
-    echo "Restoring backup..."
+    echo -e "${green}Cargando backup..."
     cd /var/www/
     tar -xvf Backup.tar.gz
     rm Backup.tar.gz
@@ -70,27 +112,49 @@ restoreBackUp(){
     yarn build:production
     sudo php artisan optimize:clear
 }
-echo "Pterodactyl Theme Installer"
-echo ""
-echo "[1] Install theme"
-echo "[2] Restore backup"
-echo "[3] Repair panel (use if you have an error in the theme installation)"
-echo "[4] Exit"
 
-read -p "Please enter a number: " choice
-if [ $choice == "1" ]
-    then
-    installThemeQuestion
-fi
-if [ $choice == "2" ]
-    then
-    restoreBackUp
-fi
-if [ $choice == "3" ]
-    then
-    repair
-fi
-if [ $choice == "4" ]
-    then
-    exit
-fi
+Main(){
+    clear
+    echo -e "${lightpurple}
+    ███╗  ██╗██╗ ██████╗ ██╗  ██╗████████╗███████╗ █████╗ ██╗     ██╗     
+    ████╗ ██║██║██╔════╝ ██║  ██║╚══██╔══╝██╔════╝██╔══██╗██║     ██║     
+    ██╔██╗██║██║██║  ██╗ ███████║   ██║   █████╗  ███████║██║     ██║     
+    ██║╚████║██║██║  ╚██╗██╔══██║   ██║   ██╔══╝  ██╔══██║██║     ██║     
+    ██║ ╚███║██║╚██████╔╝██║  ██║   ██║   ██║     ██║  ██║███████╗███████╗
+    ╚═╝  ╚══╝╚═╝ ╚═════╝ ╚═╝  ╚═╝   ╚═╝   ╚═╝     ╚═╝  ╚═╝╚══════╝╚══════╝
+    ${white} VPS Utils for Pterodactyl
+
+    ${purple}[1]${gray} Instalar Tema
+    ${purple}[2]${gray} Instalar Panel (Dependencias)
+    ${purple}[3]${gray} Instalar Wings (Dependencias)
+    ${purple}[4]${gray} Cargar Backup
+    ${purple}[5]${gray} Reparar Panel
+    ${purple}[6]${gray} Salir
+    "
+    echo -ne "${gray}[!] ${white}Elije una opcion"; read -p ": " choice
+    if [ $choice == "1" ]
+        then
+        ThemeQuestion
+    fi
+    if [ $choice == "2" ]
+        then
+        restoreBackUp
+    fi
+    if [ $choice == "3" ]
+        then
+        restoreBackUp
+    fi
+    if [ $choice == "4" ]
+        then
+        restoreBackUp
+    fi
+    if [ $choice == "5" ]
+        then
+        repair
+    fi
+    if [ $choice == "6" ]
+        then
+        exit
+    fi
+}
+Main();
